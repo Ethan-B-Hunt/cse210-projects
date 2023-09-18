6acdd9adfc8b1349d3dcc399d9a38a0003c1f189
Ethan-B-Hunt <33461555+Ethan-B-Hunt@users.noreply.github.com>
2023-09-18 10:06:20 -0600

    project_3_first

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -1,9 +1,35 @@
 using System;
+using System.Globalization;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        // Console.WriteLine("Hello Prep3 World!");
+
+        //
+        int guess_num = -1;
+        int num_of_guess = 0;
+
+        Console.WriteLine("Welcome to guess the number");
+
+        Random randomGenerator = new Random();
+        int random_number = randomGenerator.Next(1, 101);
+
+
+        while (random_number != guess_num) {
+            Console.Write("What is your guess: ");
+            string guess = Console.ReadLine();
+            guess_num = int.Parse(guess);
+            if (guess_num < random_number){
+                Console.WriteLine("Nope, Guess was too low");
+            }
+            else if (guess_num > random_number){
+                Console.WriteLine("Nope, Guess was too high");
+            }
+            num_of_guess += 1;
+       }
+       Console.WriteLine($"You Won!!!!, the number was {random_number}!");
+       Console.WriteLine($"The number of Guesses {num_of_guess}");
     }
 }
